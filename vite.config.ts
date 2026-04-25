dfc61fe565acd34098ba74c593e9c4e35077fcbb
tiouoo <tiouo@qq.com>
2026-04-26 00:21:46 +0800

    feat(vite.config): 添加robots.txt到includeAssets，优化缓存配置

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,13 @@ export default defineConfig({
         enabled: true,
         type: 'module',
       },
-      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png', 'BingSiteAuth.xml'],
+      includeAssets: [
+        'favicon.ico',
+        'icon-192.png',
+        'icon-512.png',
+        'BingSiteAuth.xml',
+        'robots.txt',
+      ],
       manifest: {
         name: 'tiouo',
         short_name: 'tiouo',
@@ -27,42 +33,23 @@ export default defineConfig({
         start_url: '/',
         scope: '/',
         icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ],
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,svg}'],
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
+        navigateFallbackAllowlist: [/^\/$/],
+        globPatterns: ['**/*.{js,css,html,ico,svg,txt,xml,png}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
             handler: 'CacheFirst',
             options: {
               cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+              cacheableResponse: { statuses: [0, 200] },
             },
           },
           {
@@ -70,10 +57,7 @@ export default defineConfig({
             handler: 'NetworkFirst',
             options: {
               cacheName: 'cdn-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24,
-              },
+              expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 },
             },
           },
           {
@@ -81,10 +65,7 @@ export default defineConfig({
             handler: 'CacheFirst',
             options: {
               cacheName: 'font-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365,
-              },
+              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
             },
           },
         ],
